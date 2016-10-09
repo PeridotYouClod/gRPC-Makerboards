b6fb7e5abe4ff394f2907598ac478b61541e165c
PeridotYouClod <imperidotyouclod@gmail.com>
2016-10-08 18:43:37 -0700

    Broke apart Server into 2 sub servers, changed Mongo.py name Added a ArduinoServer.py and WioServer.py. Fixed some qoutes

---
+++ ArduinoServer.py
@@ -0,0 +1,45 @@
+from concurrent import futures
+import time
+import grpc
+from Sensor import ArduinoSensorReader
+import ProtoConfig
+import re
+import sys
+
+import proto_out.sensors_pb2 as sensors_pb2
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+PORT = 50052
+
+class Arduino(sensors_pb2.ArduinoServicer):
+  def __init__(self):
+    super().__init__()
+    protoConfig = ProtoConfig.getConfig()
+    arduino = protoConfig.arduinos[0]
+    self.sensorReader = ArduinoSensorReader(arduino)
+
+  def GetIrButtonPressed(self, request, context):
+    clean_value = self.sensorReader.GetCurrentValue()
+    print('Returning Button Press: %s' % clean_value)
+    return sensors_pb2.GetIrButtonPressedReply(button=clean_value)
+
+  def GetSonar(self, request, context):
+    clean_value = self.sensorReader.GetCurrentValue()
+    print('Returning Sonar: %s' % clean_value)
+    return sensors_pb2.GetSonarReply(distance=clean_value)
+
+def serve():
+  server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+  sensors_pb2.add_ArduinoServicer_to_server(Arduino(), server)
+  server.add_insecure_port('[::]:%s' % PORT)
+  server.start()
+  print('Server Started on Port %s ' % PORT)
+
+  try:
+    while True:
+      time.sleep(_ONE_DAY_IN_SECONDS)
+  except KeyboardInterrupt:
+    server.stop(0)
+
+if __name__ == '__main__':
+  serve()
--- WioServer.py
+++ WioServer.py
@@ -1,19 +1,20 @@
 from concurrent import futures
 import time
-import Reader
 import grpc
-from Sensor import WioSensorReader
-import ProtoConfig
 import serial
 import re
 import sys
 
+from Sensor import WioSensorReader
+import Mongo
+import ProtoConfig
 import proto_out.sensors_pb2 as sensors_pb2
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
+PORT = 50051
 
 
-class Sensors(sensors_pb2.SensorsServicer):
+class WioLink(sensors_pb2.WioLinkServicer):
   def __init__(self):
     super().__init__()
     protoConfig = ProtoConfig.getConfig()
@@ -30,13 +31,13 @@ class Sensors(sensors_pb2.SensorsServicer):
 
   def GetLux(self, request, context):
     lux = self.lux.GetCurrentValue()
-    print("Returning Brigtness: %s" % lux)
+    print('Returning Brigtness: %s' % lux)
     return sensors_pb2.GetLuxReply(lux=lux['lux'])
 
   def GetTemperature(self, request, context):
     temperature_c = self.temperature_c.['temperature_c']
     temperature_f = float(temperature_c) * (9./5.) + 32.
-    print("Returning Temperature: %s*c / %s*f" %
+    print('Returning Temperature: %s*c / %s*f' %
       (temperature_c, temperature_f))
     return sensors_pb2.GetTemperatureReply(
       temperature_c=temperature_c,
@@ -44,40 +45,40 @@ class Sensors(sensors_pb2.SensorsServicer):
 
   def GetSound(self, request, context):
     sound = self.sound.GetCurrentValue()
-    print("Returning Loudness: %s" % sound)
-    return sensors_pb2.GetLoudnessReply(loudness=sound['loudness'])
+    print('Returning Loudness: %s' % sound)
+    return sensors_pb2.GetSoundReply(loudness=sound['loudness'])
 
   def GetIrButtonPressed(self, request, context):
     serial_value = self.ser.readline()
     clean_value = int(serial_value)
-    print("Returning Button Press: %s" % clean_value)
+    print('Returning Button Press: %s' % clean_value)
     return sensors_pb2.GetIrButtonPressedReply(button=clean_value)
 
   def GetSonar(self, request, context):
     serial_value = self.ser.readline()
     clean_value = int(serial_value)
     self.ser.reset_input_buffer()
-    print("Returning Sonar: %s" % clean_value)
+    print('Returning Sonar: %s' % clean_value)
     return sensors_pb2.GetSonarReply(distance=clean_value)
 
   def SetLedStrip(self, req, context):
-    url = "/%s/%s/%s" % (req.length, req.brightness, req.speed)
+    url = '/%s/%s/%s' % (req.length, req.brightness, req.speed)
     self.ledStrip.setUrl(url)
     return sensors_pb2.SetLedStripReply()
 
  def GetButtonPressed(self, req, context):
    pressedVal = self.button.GetCurrentValue()
    pressed = pressedVal['pressed']
-   print("Returning Pressed: %s" % pressedVal)
+   print('Returning Pressed: %s' % pressedVal)
    val = True if pressed == 1 else False
-   print("Returning Pressed: %s" % val)
+   print('Returning Pressed: %s' % val)
    return sensors_pb2.GetButtonPressedReply(pressed=val)
 
 def serve():
   server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
   sensors_pb2.add_SensorsServicer_to_server(Sensors(), server)
-  server.add_insecure_port('[::]:50051')
-  print('Starting Server on Port 50051')
+  server.add_insecure_port('[::]:%s' % PORT)
+  print('Starting Server on Port %s' % PORT)
   server.start()
 
   try:
