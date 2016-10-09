312cfc60253f4e85e209757b4b291e9415b564c9
PeridotYouClod <imperidotyouclod@gmail.com>
2016-10-08 18:52:28 -0700

    Update README.md, repalce " with '

--- README.md
+++ README.md
@@ -1,12 +1,43 @@
 # gRPC Makerboards
 
-## Runable Files
+## Runnable Files
+### Servers (Implemented in Python however gRPC allows any language implemtation)
+* WioServer.py - gRPC Server connecting to a [Wio Link](http://wiki.seeed.cc/Wio_Link/) via REST.
+* ArduinoServer.py - gRPC Server connecting to an [Arduino](https://www.arduino.cc/en/Guide/Introduction) via COM port.
+* FrontEndServer.py - gRPC Server communicates to backend servers to give a single connection point to the client.
+* Client.py - Simple gRPC Client example.
+
+### Automatic Database Storage
 * Reader - Reads the values from the Sensor and stores them to a Mongo Database.
+
+### Visualization
 * Graph - Reads the values from the Mongo Database and Graphs it.
-* WioServer - Sensor gRPC Server impleneted in Python.
-* WioClient - Simple Sensor gRPC Client impleneted in Python.
+
+## Set Up Environment
+
+    sudo apt-get install git
+
+    # Install mongo and set up database folder
+    sudo apt-get install mongodb
+    sudo mkdir /data/db
+    sudo chown <YOUR USERNAME> -R /data/
+
+    # Install python3 pip if it is missing
+    sudo apt-get install python3-pip
+    pip3 install --upgrade pip
+
+    # Grab gRPC for protobufs and servers
+    sudo pip3 install grpcio
+    sudo pip3 install grpcio-tools
+
+    # Get required python libs
+    pip3 install pymongo
+    pip3 install urllib3
+    pip3 install plotly
 
 ## Build the Protobufs
+ Every time a .proto file is changed run this script to create new implementations of the edited protobuf or gRPC Server.
+ 
     python -m grpc.tools.protoc \
       ./protos/sensors.proto ./protos/config.proto \
       --proto_path=./protos \
--- Sensor.py
+++ Sensor.py
@@ -1,5 +1,6 @@
 import urllib.request
 import json
+import serial
 
 class WioSensorReader(object):
   def __init__(self, wioConfig, groveConfig):
@@ -17,7 +18,7 @@ class WioSensorReader(object):
       try:
         print(self.getUrl())
         request = urllib.request.urlopen(self.getUrl())
-        return_val = request.read().decode("utf-8")
+        return_val = request.read().decode('utf-8')
         request.close()
         return json.loads(return_val)
       except:
@@ -26,7 +27,7 @@ class WioSensorReader(object):
   def setUrl(self, urlArgsStr):
       url = (self.wioConfig.rootUrl + self.groveConfig.apiMethod
         + urlArgsStr + self.wioConfig.apiKey)
-      print("setUrl: ", url)
+      print('setUrl: ', url)
       params = urllib.parse.urlencode({}).encode('utf-8')
       urllib.request.urlopen(url, data=params)
 
@@ -36,21 +37,22 @@ class WioSensorReader(object):
 class ArduinoSensorReader(object):
   def __init__(self, arduinoConfig):
     self.config = arduinoConfig
-    self.serial = serial.Serial(arduinoConfig.comPort, arduinoConfig.baudRate)
+    self.serial = serial.Serial(
+      arduinoConfig.comPort, arduinoConfig.baudRate)
 
   def getName(self):
-    return "Arduino on %s" % arduinoConfig.comPort
+    return 'Arduino on %s' % arduinoConfig.comPort
 
   def GetCurrentValue(self):
     serial_value = self.ser.readline()
     clean_value = int(serial_value)
     self.ser.reset_input_buffer()
-    print("Returning Arduino Value: %s" % clean_value)
+    print('Returning Arduino Value: %s' % clean_value)
     return clean_value
 
 class SensorDbWriter(object):
   def __init__(self, sensor, db_col):
-    print("sensor: %s\ndb_col: %s" % (sensor, db_col))
+    print('sensor: %s\ndb_col: %s' % (sensor, db_col))
     self.sensor = sensor
     self.db_col = db_col
 
@@ -58,8 +60,8 @@ class SensorDbWriter(object):
     current_val = self.sensor.GetCurrentValue()
     if current_val:
       current_val['date'] = date
-      print("Inserting: %s" % current_val)
+      print('Inserting: %s' % current_val)
       self.db_col.insert_one(current_val)
     else:
-      print("Faied to get value for %s" % self.sensor.getName())
+      print('Faied to get value for %s' % self.sensor.getName())
     return current_val
