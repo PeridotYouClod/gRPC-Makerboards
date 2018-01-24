eecea9ffcd05ea299106123467cfcb9536ec2137
PeridotYouClod <imperidotyouclod@gmail.com>
2018-01-23 20:20:51 -0800

    Updated WriteToButtonRemote to support all 5 buttons

--- arduino/WriteToButtonRemote/WriteToButtonRemote.ino
+++ arduino/WriteToButtonRemote/WriteToButtonRemote.ino
@@ -6,21 +6,46 @@
 
 String possibleSerial;
 char incomingByte;
-
-int rfOnButton, rfOffButton, buttonPressDelay;
-
 bool debug;
+
+// Set up pins
+int RF_ON_1 = 2;
+int RF_OFF_1 = 3;
+
+int RF_ON_2 = 4;
+int RF_OFF_2 = 5;
+
+int RF_ON_3 = 6;
+int RF_OFF_3 = 7;
+
+int RF_ON_4 = 8;
+int RF_OFF_4 = 9;
+
+int RF_ON_5 = 10;
+int RF_OFF_5 = 11;
+
+// Length of time to hold down button
+int BUTTON_PRESS_DELAY = 500;
 
 void setup() {
   debug = false;
   
   Serial.begin(9600);
-  rfOffButton = 2;
-  rfOnButton = 3;
-  buttonPressDelay = 500;
   
-  pinMode(rfOnButton, OUTPUT);
-  pinMode(rfOffButton, OUTPUT);
+  pinMode(RF_ON_1, OUTPUT);
+  pinMode(RF_OFF_1, OUTPUT);
+  
+  pinMode(RF_ON_2, OUTPUT);
+  pinMode(RF_OFF_2, OUTPUT);
+  
+  pinMode(RF_ON_3, OUTPUT);
+  pinMode(RF_OFF_3, OUTPUT);
+  
+  pinMode(RF_ON_4, OUTPUT);
+  pinMode(RF_OFF_4, OUTPUT);
+  
+  pinMode(RF_ON_5, OUTPUT);
+  pinMode(RF_OFF_5, OUTPUT);
 }
 
 void loop() {
@@ -29,33 +54,37 @@ void loop() {
     // read the incoming byte:
     incomingByte = (char)(Serial.read());
     possibleSerial += incomingByte;
+    delay(10);
   }
   if(possibleSerial.length()) {
     debugPrint(possibleSerial);
   //  switch for checking each of our opcodes
     switch(possibleSerial.toInt()) {
   // 3 bits for address of button, 1 bit for state
-      case 101:  // turn on the radio button
-        debugPrint("turning on the rf button");
-        digitalWrite(rfOnButton, HIGH);
-        delay(buttonPressDelay);
-        digitalWrite(rfOnButton, LOW);
-        break;
-      case 100:  // turn off the radio button
-        debugPrint("turning off the rf button");
-        digitalWrite(rfOffButton, HIGH);
-        delay(buttonPressDelay);
-        digitalWrite(rfOffButton, LOW);
-        break;
+      case 100: pressButton(RF_OFF_1); break;
+      case 101: pressButton(RF_ON_1); break;
+      case 200: pressButton(RF_OFF_2); break;
+      case 201: pressButton(RF_ON_2); break;
+      case 300: pressButton(RF_OFF_3); break;
+      case 301: pressButton(RF_ON_3); break;
+      case 400: pressButton(RF_OFF_4); break;
+      case 401: pressButton(RF_ON_4); break;
+      case 500: pressButton(RF_OFF_5); break;
+      case 501: pressButton(RF_ON_5); break;
       default:
         debugPrint("bad data sent " + possibleSerial);
     }
   }
 
-
-//  cleanup for next possible data input check
+  // cleanup for next possible data input check
   possibleSerial = "";
   delay(50);
+}
+
+void pressButton(int button) {
+  digitalWrite(button, HIGH);
+  delay(BUTTON_PRESS_DELAY);
+  digitalWrite(button, LOW);
 }
 
 void debugPrint(String message) {
